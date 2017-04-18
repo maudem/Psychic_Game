7a0a1d63ba04141ab13ab807e5e4c75393964b14
Maude <mualth@gmail.com>
2017-04-17 21:29:03 -0400

    buggy but working javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,66 @@
+var game = {
+ choices : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+ wins : 0,
+ losses : 0,
+ tries : 5
+}
 
-// ===============My Pseudocode ==============
+$(document).ready(function(){
 //The Computer guesses a random number from 0-9
 //Capture choices
+ 	var compChoice = game.choices[Math.floor(Math.random()*game.choices.length)];
+ 
 
-// The User tries to choose the right number
-// Capture choices
+//
 
-// If the User chooses right, the message "Correct!!" displays, the round resets and 1 point is added to 'wins'
+//The User tries to choose the right number
 
-// If the User chooses wrong, the entry is displayed in 'Guesses so far' and 1 point is subtracted to 'Tries so far'
+	//resets round 
+	var newRound = function() {
+		game.wins();
+		game.losses();
+		game.tries = 5;
+	}
 
-// After 5 wrong tries (or 'Tries so far'=0) Computer guess is displayed, 1 point is addled to 'losses' and round resets
+	$("#userGuess").click(function(){
+		//sets the inputted value to userGuess
+		var userGuess = $("input").val();
 
+		//put in validation for numbers 0-9
+		if (compChoice === userGuess){
+			alert("Correct!!!"); //alert when guessed right
+			game.wins +=1;
+		}
+
+		else {  //alert when guessed wrong
+			game.tries-=1;
+			alert("Try Again. You have " + game.tries + " attemts left.  Concentrate!!");
+		}
+
+		if (game.tries == 0){
+			game.losses+=1;
+			alert("Sorry, the right answer is " + compChoice);
+			newRound();
+		}
+		//append current score to appropriate ids
+		$("#wins").empty();
+		$("#losses").empty();
+
+		$("#wins").append("Wins: " + game.wins);
+		$("#losses").append("Losses: " + game.losses);
+
+		
+	});
+
+	
+
+});
+
+
+	// 	}
+	// }
+// After 5 wrong tries (or 'Tries so far'=0) Computer guess is displayed, 1 point is added to 'losses' and round resets
+//}
 // After 5 rounds, game is over and
 
 // 5 or 4 wins display "clarvoyant master!"
@@ -20,31 +69,14 @@
 
 
 //array of chioces 0-9
-var choices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-var wins = 0;
-var losses = 0;
-var tries=5;
 //capturing and logging the user guess try to hook it to the gues button
-function checkguess(){
-document.onkeyup = function() {
-	var userGuess = document.getElementById('guess').value;}
-		console.log(userGuess);
 	
 
 //the computer picks a random number and it is logged
-	var compChoice = choices[Math.floor(Math.random()*choices.length)];
-		console.log(compChoice);
+	
 
 
-var checkGuess = function () {
 
-	if (userGuess === compChoice){
-		 alert ("Yes!! Your are a clarvoyant master!");  
-	}
-	else {
-		alert ("Concentrate!  Try again.");
-		
-	}
-
-}
+	
+	
